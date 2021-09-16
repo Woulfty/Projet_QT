292da5ebed45b9664b6fea345f5ff58e7ffcf2ba
julienlaridant <70692611+julienlaridant@users.noreply.github.com>
2021-09-16 14:40:22 +0200

    Add files via upload

--- tcp/tcp/main.cpp
+++ tcp/tcp/main.cpp
@@ -1,1 +1,34 @@
-#include "main.h"
+
+
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	WSADATA data;
+	WORD version = MAKEWORD(2, 2);
+	int ws0k = WSAStartup(version, &data);
+	if (ws0k != 0) {
+		cout << "chehhh" << ws0k;
+
+
+	}
+
+	SOCKET in = socket(AF_INET, SOCK_DGRAM,0);
+	sockaddr_in serverHint;
+	serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
+	serverHint.sin_family = AF_INET;
+	serverHint
+
+
+
+int ws0k = WSAStartup(version,&data);
+int 
+
+}
+
+
